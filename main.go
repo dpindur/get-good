a86cd553f7b11aa1663a345f75017020c073348d
Dylan Pindur <dpindur@users.noreply.github.com>
2018-08-10 13:52:41 +0800

    Add basic command line argument parsing

--- main.go
+++ main.go
@@ -1,10 +1,80 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	workerCount := flag.Int("workers", 5, "number of worker threads")
+	clearDB := flag.Bool("clear-db", false, "clear the database before starting")
+	dbFile := flag.String("db", "bust.db", "database file to store results")
+	urlStr := flag.String("url", "", "url to perform directory bust against")
+	wordsFile := flag.String("wordlist", "", "wordlist file to use")
+	extensionsFlag := flag.String("extensions", "html,php", "comma separated list of extensions to append")
+
+	flag.Parse()
+	flagsInvalid := false
+
+	// Workers
+	if *workerCount < 1 {
+		fmt.Println("please specify 1 or more worker threads")
+		flagsInvalid = true
+	}
+
+	// Database File
+	if !strings.HasSuffix(*dbFile, ".db") {
+		*dbFile += ".db"
+	}
+	dbFilePath, err := filepath.Abs(*dbFile)
+	if err != nil {
+		fmt.Println("error resolving path %v\n", *dbFile)
+		flagsInvalid = true
+	}
+
+	// Url
+	urlProvided := true
+	if *urlStr == "" {
+		fmt.Println("please provide a URL to perform the directory bust against")
+		flagsInvalid = true
+		urlProvided = false
+	}
+	if !strings.HasSuffix(*urlStr, "/") {
+		*urlStr += "/"
+	}
+	_, err = url.ParseRequestURI(*urlStr)
+	if err != nil && urlProvided {
+		fmt.Println("error parsing url, please ensure it includes the protocol for example http://google.com/")
+		flagsInvalid = true
+	}
+
+	// Wordlist
+	if *wordsFile == "" {
+		fmt.Println("please provide a wordlist file")
+		flagsInvalid = true
+	}
+	wordsFilePath, err := filepath.Abs(*wordsFile)
+	if err != nil {
+		fmt.Println("error resolving path %v\n", *wordsFile)
+		flagsInvalid = true
+	}
+
+	// Extensions
+	splitExtensions := strings.Split(*extensionsFlag, ",")
+	extensions := make([]string, 0)
+	extensions = append(extensions, "")
+	extensions = append(extensions, splitExtensions...)
+
+	if flagsInvalid {
+		os.Exit(1)
+	}
+
+	_ = clearDB
+	_ = dbFilePath
+	_ = wordsFilePath
 }
